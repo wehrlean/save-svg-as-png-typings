6a9231fd190c4a9f14cff52a033482b102ec2eb7
wehrlean <49229000+wehrlean@users.noreply.github.com>
2020-06-03 16:28:00 +0200

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 declare module "save-svg-as-png" {
-  export function prepareSvg(el: any, options?: any, done?: any): Promise<;
+  export function prepareSvg(el: any, options?: any, done?: any): void;
   export function svgAsDataUri(el: any, options?: any): Promise<String>;
   export function svgAsPngUri(el: any, options?: any): Promise<String>;
   export function saveSvg(el: any, name: string, options?: any): void;
